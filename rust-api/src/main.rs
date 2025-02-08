ca3f84bd69e4321175b5361e79c08438ae0f3ce2
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-08 14:33:31 -0600

    Add rust-api.

---
+++ rust-api/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/health", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:18080")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "all good"
+}
