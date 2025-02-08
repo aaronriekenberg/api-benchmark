bda38a87a38d42ad5737e69312aa26c554b70310
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-08 13:47:56 -0600

    NUM_THREADS 1 2 4

--- run.sh
+++ run.sh
@@ -13,19 +13,25 @@ echo "before go build"
 go build
 echo "after go build"
 
-./go-api &
-GO_API_PID=$!
+for NUM_THREADS in 1 2 4; do
 
-echo "go-api running PID $GO_API_PID"
+    echo "NUM_THREADS=$NUM_THREADS"
 
-echo "wrk --latency -t1 -c100 -d10s http://localhost:18080/health"
-wrk --latency -t1 -c100 -d10s http://localhost:18080/health
+    ./go-api &
+    GO_API_PID=$!
 
-echo ps -eLf -q $GO_API_PID
-ps -eLf -q $GO_API_PID
+    echo "go-api running PID $GO_API_PID"
 
-echo ps -eo pid,user,rss,time -q $GO_API_PID
-ps -eo pid,user,rss,time -q $GO_API_PID
+    echo "wrk --latency -t$NUM_THREADS -c100 -d10s http://localhost:18080/health"
+    wrk --latency -t$NUM_THREADS -c100 -d10s http://localhost:18080/health
 
-echo kill $GO_API_PID
-kill $GO_API_PID
+    echo ps -eLf -q $GO_API_PID
+    ps -eLf -q $GO_API_PID
+
+    echo ps -eo pid,user,rss,time -q $GO_API_PID
+    ps -eo pid,user,rss,time -q $GO_API_PID
+
+    echo kill $GO_API_PID
+    kill $GO_API_PID
+
+done
