fc7694eabc2f01d046ca272887ea0692bccde7f7
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-08 05:54:03 -0600

    Add run.sh.

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#set -x
+
+echo "begin run.sh"
+
+lscpu
+
+echo
+
+cd go-api
+go build -x
+
+./go-api &
+GO_API_PID=$!
+
+echo "go-api running PID $GO_API_PID"
+
+echo "h2load --h1 -t8 -c8 -n1000000 'http://localhost:18080/health'"
+h2load --h1 -t8 -c8 -n1000000 'http://localhost:18080/health'
+
+echo ps -eLf -q $GO_API_PID
+ps -eLf -q $GO_API_PID
+
+echo ps -eo pid,user,rss,time -q $GO_API_PID
+ps -eo pid,user,rss,time -q $GO_API_PID
+
+echo kill $GO_API_PID
+kill $GO_API_PID
