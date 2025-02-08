962efab7259d96fd9ef2ede267f46d745551c357
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-08 05:37:20 -0600

    Add code.

---
+++ go-api/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"runtime/debug"
+	"strings"
+)
+
+func healthHandlerFunc() http.HandlerFunc {
+	const responseBodyString = "all good"
+
+	responseBodyBytes := []byte(responseBodyString)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		io.Copy(w, bytes.NewReader(responseBodyBytes))
+	}
+}
+
+func runHTTPServer() {
+	const addr = ":8080"
+
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /health", healthHandlerFunc())
+
+	slog.Info("starting server",
+		"addr", addr,
+	)
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    addr,
+	}
+	server.ListenAndServe()
+}
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			slog.Error("panic in main",
+				"error", err,
+			)
+			fmt.Fprintf(os.Stderr, "stack trace:\n%v", string(debug.Stack()))
+			os.Exit(1)
+		}
+	}()
+
+	setupSlog()
+
+	slog.Info("begin main",
+		"buildInfoMap", buildInfoMap(),
+		"goEnvironVariables", goEnvironVariables(),
+	)
+
+	runHTTPServer()
+}
+
+func setupSlog() {
+	level := slog.LevelInfo
+
+	if levelString, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		err := level.UnmarshalText([]byte(levelString))
+		if err != nil {
+			panic(fmt.Errorf("level.UnmarshalText error %w", err))
+		}
+	}
+
+	slog.SetDefault(
+		slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{
+					Level: level,
+				},
+			),
+		),
+	)
+
+	slog.Info("setupSlog",
+		"configuredLevel", level,
+	)
+}
+
+func buildInfoMap() map[string]string {
+	buildInfoMap := make(map[string]string)
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		buildInfoMap["GoVersion"] = buildInfo.GoVersion
+		for _, setting := range buildInfo.Settings {
+			if strings.HasPrefix(setting.Key, "GO") ||
+				strings.HasPrefix(setting.Key, "vcs") {
+				buildInfoMap[setting.Key] = setting.Value
+			}
+		}
+	}
+
+	return buildInfoMap
+}
+
+func goEnvironVariables() []string {
+	var goVars []string
+	for _, env := range os.Environ() {
+		if strings.HasPrefix(env, "GO") {
+			goVars = append(goVars, env)
+		}
+	}
+	return goVars
+}
