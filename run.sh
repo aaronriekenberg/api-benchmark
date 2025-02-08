a1d6daca4e22ef789ff04a0b3c1976f10e15945a
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-08 13:40:51 -0600

    Add setup.sh, use wrk.

--- run.sh
+++ run.sh
@@ -6,9 +6,6 @@ echo "begin run.sh"
 
 lscpu
 
-echo sudo apt install -y nghttp2
-sudo apt install -y nghttp2
-
 echo
 
 cd go-api
@@ -21,8 +18,8 @@ GO_API_PID=$!
 
 echo "go-api running PID $GO_API_PID"
 
-echo "h2load --h1 -t8 -c8 -n1000000 'http://localhost:18080/health'"
-h2load --h1 -t8 -c8 -n1000000 'http://localhost:18080/health'
+echo "wrk --latency -t1 -c100 -d10s http://localhost:18080/health"
+wrk --latency -t1 -c100 -d10s http://localhost:18080/health
 
 echo ps -eLf -q $GO_API_PID
 ps -eLf -q $GO_API_PID
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "begin setup.sh"
+
+echo sudo apt install -y wrk
+sudo apt install -y wrk
