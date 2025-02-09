44002eda462a555968dc09968e853cfe8492bdf1
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-09 16:42:19 -0600

    Use oha for http2 benchmark.

--- run-api-benchmark.sh
+++ run-api-benchmark.sh
@@ -9,59 +9,51 @@ lsmem
 
 echo
 
-DURATION=20s
+$API_NAME &
+API_PID=$!
 
-for NUM_THREADS in 1 2; do
+sleep 1
 
-    for NUM_CONNECTIONS in 100 200 400 800; do
+echo "$TEST_NAME running PID $API_PID"
 
-        echo "NUM_CONNECTIONS=$NUM_CONNECTIONS NUM_THREADS=$NUM_THREADS"
+echo "oha-linux-amd64 --http2 -n 1000000 -c 16 -p 100 --no-tui --json 'http://localhost:18080/test'"
+oha-linux-amd64 --http2 -n 1000000 -c 16 -p 100 --no-tui --json 'http://localhost:18080/test'
 
-        $API_NAME &
-        API_PID=$!
+# rm -f wrk_output
+# echo "wrk --latency -d$DURATION -t$NUM_THREADS -c$NUM_CONNECTIONS 'http://localhost:18080/test'"
+# wrk --latency -d$DURATION -t$NUM_THREADS -c$NUM_CONNECTIONS 'http://localhost:18080/test' 2>&1 | tee wrk_output
 
-        sleep 1
+# RPS=$(cat wrk_output | grep 'Requests/sec:' | awk '{print $2}' )
+# echo "RPS = $RPS"
 
-        echo "$TEST_NAME running PID $API_PID"
+# REQUEST_P50=$(cat wrk_output | grep -A5 'Latency Distribution' | grep '50%' | awk '{print $2}' )
+# echo "REQUEST_P50 = $REQUEST_P50"
 
-        rm -f wrk_output
-        echo "wrk --latency -d$DURATION -t$NUM_THREADS -c$NUM_CONNECTIONS 'http://localhost:18080/test'"
-        wrk --latency -d$DURATION -t$NUM_THREADS -c$NUM_CONNECTIONS 'http://localhost:18080/test' 2>&1 | tee wrk_output
+# REQUEST_P99=$(cat wrk_output | grep -A5 'Latency Distribution' | grep '99%' | awk '{print $2}' )
+# echo "REQUEST_P99 = $REQUEST_P99"
 
-        RPS=$(cat wrk_output | grep 'Requests/sec:' | awk '{print $2}' )
-        echo "RPS = $RPS"
+# echo ps -eLf -q $API_PID
+# ps -eLf -q $API_PID
 
-        REQUEST_P50=$(cat wrk_output | grep -A5 'Latency Distribution' | grep '50%' | awk '{print $2}' )
-        echo "REQUEST_P50 = $REQUEST_P50"
+# THREADS_IN_APP=$(ps -eLf -q $API_PID | grep -v PID | wc -l)
+# echo "THREADS_IN_APP=$THREADS_IN_APP"
 
-        REQUEST_P99=$(cat wrk_output | grep -A5 'Latency Distribution' | grep '99%' | awk '{print $2}' )
-        echo "REQUEST_P99 = $REQUEST_P99"
+# echo ps -eo pid,user,rss,time -q $API_PID
+# ps -eo pid,user,rss,time -q $API_PID
 
-        echo ps -eLf -q $API_PID
-        ps -eLf -q $API_PID
+# RSS_KB=$(ps -eo pid,user,rss,time -q $API_PID | tail -1 | awk '{print $3}' )
+# echo "RSS_KB=$RSS_KB"
 
-        THREADS_IN_APP=$(ps -eLf -q $API_PID | grep -v PID | wc -l)
-        echo "THREADS_IN_APP=$THREADS_IN_APP"
+# CPU_TIME=$(ps -eo pid,user,rss,time -q $API_PID | tail -1 | awk '{print $4}' )
+# echo "CPU_TIME=$CPU_TIME"
 
-        echo ps -eo pid,user,rss,time -q $API_PID
-        ps -eo pid,user,rss,time -q $API_PID
+# echo kill $API_PID
+# kill $API_PID
 
-        RSS_KB=$(ps -eo pid,user,rss,time -q $API_PID | tail -1 | awk '{print $3}' )
-        echo "RSS_KB=$RSS_KB"
+# echo "$TEST_NAME,$DURATION,$NUM_CONNECTIONS,$NUM_THREADS,$RPS,$REQUEST_P50,$REQUEST_P99,$RSS_KB,$CPU_TIME,$THREADS_IN_APP" >> $OUTPUT_FILE
 
-        CPU_TIME=$(ps -eo pid,user,rss,time -q $API_PID | tail -1 | awk '{print $4}' )
-        echo "CPU_TIME=$CPU_TIME"
+# sleep 1
 
-        echo kill $API_PID
-        kill $API_PID
 
-        echo "$TEST_NAME,$DURATION,$NUM_CONNECTIONS,$NUM_THREADS,$RPS,$REQUEST_P50,$REQUEST_P99,$RSS_KB,$CPU_TIME,$THREADS_IN_APP" >> $OUTPUT_FILE
-
-        sleep 1
-
-    done
-
-done
-
-echo "after tests cat $OUTPUT_FILE"
-cat $OUTPUT_FILE
+# echo "after tests cat $OUTPUT_FILE"
+# cat $OUTPUT_FILE
--- setup.sh
+++ setup.sh
@@ -4,13 +4,15 @@ set -e
 
 echo "begin setup.sh"
 
-echo sudo apt install -y wrk
-sudo apt install -y wrk
+wget https://github.com/hatoo/oha/releases/download/v1.7.0/oha-linux-amd64
+chmod +x oha-linux-amd64
 
-OUTPUT_FILE=results/latest.csv
-echo "OUTPUT_FILE=$OUTPUT_FILE"
+ls -altrh oha-linux-amd64
 
-rm -f $OUTPUT_FILE
-echo "TEST_NAME,DURATION,NUM_CONNECTIONS,NUM_THREADS,RPS,REQUEST_P50,REQUEST_P99,RSS_KB,CPU_TIME,THREADS_IN_APP" >> $OUTPUT_FILE
-echo "created CSV header OUTPUT_FILE=$OUTPUT_FILE"
-cat $OUTPUT_FILE
+# OUTPUT_FILE=results/latest.csv
+# echo "OUTPUT_FILE=$OUTPUT_FILE"
+
+# rm -f $OUTPUT_FILE
+# echo "TEST_NAME,DURATION,NUM_CONNECTIONS,NUM_THREADS,RPS,REQUEST_P50,REQUEST_P99,RSS_KB,CPU_TIME,THREADS_IN_APP" >> $OUTPUT_FILE
+# echo "created CSV header OUTPUT_FILE=$OUTPUT_FILE"
+# cat $OUTPUT_FILE
