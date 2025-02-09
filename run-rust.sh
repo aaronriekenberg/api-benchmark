dd3545d402cf399e798fcfda5736c57dc5c4f195
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-09 11:36:58 -0600

    Use wrk.

--- run-rust.sh
+++ run-rust.sh
@@ -21,11 +21,11 @@ echo "after cargo build"
 cd -
 echo "pwd = $(pwd)"
 
-NUM_REQUESTS=1000000
+DURATION=10s
 
-for NUM_THREADS in 4 8 16; do
+for NUM_THREADS in 4; do
 
-    for NUM_CONNECTIONS in 100 200 400; do
+    for NUM_CONNECTIONS in 100; do
 
         echo "NUM_CONNECTIONS=$NUM_CONNECTIONS NUM_THREADS=$NUM_THREADS"
 
@@ -36,21 +36,18 @@ for NUM_THREADS in 4 8 16; do
 
         echo "rust-api running PID $API_PID"
 
-        rm -f h2load_output
-        echo "h2load --h1 -n$NUM_REQUESTS -t$NUM_THREADS -c$NUM_CONNECTIONS -m1 'http://localhost:18080/test'"
-        h2load --h1 -n$NUM_REQUESTS -t$NUM_THREADS -c$NUM_CONNECTIONS -m1 'http://localhost:18080/test' 2>&1 | tee h2load_output
+        rm -f wrk_output
+        echo "wrk --latency -d$DURATION -t$NUM_THREADS -c$NUM_CONNECTIONS 'http://localhost:18080/test'"
+        wrk --latency -d$DURATION -t$NUM_THREADS -c$NUM_CONNECTIONS 'http://localhost:18080/test' 2>&1 | tee wrk_output
 
-        RPS=$(cat h2load_output | grep 'finished in' | awk '{print $4}' )
+        RPS=$(cat wrk_output | grep 'Requests/sec:' | awk '{print $2}' )
         echo "RPS = $RPS"
 
-        REQUEST_MAX=$(cat h2load_output | grep 'time for request: ' | awk '{print $5}' )
-        echo "REQUEST_MAX = $REQUEST_MAX"
-
-        REQUEST_MEAN=$(cat h2load_output | grep 'time for request: ' | awk '{print $6}' )
+        REQUEST_P50=$(cat wrk_output | grep '50%' | awk '{print $2}' )
         echo "REQUEST_MEAN = $REQUEST_MEAN"
 
-        REQUEST_SD=$(cat h2load_output | grep 'time for request: ' | awk '{print $7}' )
-        echo "REQUEST_SD = $REQUEST_SD"
+        REQUEST_P99=$(cat wrk_output | grep '99%' | awk '{print $2}' )
+        echo "REQUEST_P99 = $REQUEST_P99"
 
         echo ps -eLf -q $API_PID
         ps -eLf -q $API_PID
@@ -70,11 +67,9 @@ for NUM_THREADS in 4 8 16; do
         echo kill $API_PID
         kill $API_PID
 
-        echo "$TEST_NAME,$NUM_REQUESTS,$NUM_CONNECTIONS,$NUM_THREADS,$RPS,$REQUEST_MAX,$REQUEST_MEAN,$REQUEST_SD,$RSS_KB,$CPU_TIME,$THREADS_IN_APP" >> $OUTPUT_FILE
+        echo "$TEST_NAME,$DURATION,$NUM_CONNECTIONS,$NUM_THREADS,$RPS,$REQUEST_P50,$REQUEST_P99,$RSS_KB,$CPU_TIME,$THREADS_IN_APP" >> $OUTPUT_FILE
 
         sleep 1
-
-
     done
 
 done
--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ set -e
 
 echo "begin setup.sh"
 
-echo sudo apt install -y nghttp2
-sudo apt install -y nghttp2
+echo sudo apt install -y wrk
+sudo apt install -y wrk
 
 OUTPUT_FILE=results/latest.csv
 echo "OUTPUT_FILE=$OUTPUT_FILE"
 
 rm -f $OUTPUT_FILE
-echo "TEST_NAME,NUM_REQUESTS,NUM_CONNECTIONS,NUM_THREADS,RPS,REQUEST_MAX,REQUEST_MEAN,REQUEST_SD,RSS_KB,CPU_TIME,THREADS_IN_APP" >> $OUTPUT_FILE
+echo "TEST_NAME,DURATION,NUM_CONNECTIONS,NUM_THREADS,RPS,REQUEST_P50,REQUEST_P99,RSS_KB,CPU_TIME,THREADS_IN_APP" >> $OUTPUT_FILE
 echo "created CSV header OUTPUT_FILE=$OUTPUT_FILE"
 cat $OUTPUT_FILE
