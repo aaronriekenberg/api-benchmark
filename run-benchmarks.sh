cb2e04aaa841d11feb13dd5e72692a91ae2a26df
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-15 11:18:51 -0600

    Enable go and rust.

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -7,36 +7,37 @@ echo "begin run-benchmarks.sh"
 export OUTPUT_FILE=results/latest.csv
 echo "OUTPUT_FILE=$OUTPUT_FILE"
 
-# # build go
-# cd go-api
-# echo "before go build"
-# go build
-# echo "after go build"
-# cd -
-# echo "pwd = $(pwd)"
+# build go
+cd go-api
+echo "before go build"
+go build
+echo "after go build"
+cd -
+echo "pwd = $(pwd)"
 
-# # run go benchmarks
-# export API_COMMAND='./go-api/go-api'
-# export TEST_NAME=go-api
-# ./run-api-benchmark.sh
+# run go benchmarks
+export API_COMMAND='./go-api/go-api'
+export TEST_NAME=go-api
+./run-api-benchmark.sh
 
-# # build rust
-# cd rust-api
-# echo "before cargo build"
-# cargo build --release
-# echo "after cargo build"
-# cd -
-# echo "pwd = $(pwd)"
+# build rust
+cd rust-api
+echo "before cargo build"
+cargo build --release
+echo "after cargo build"
+cd -
+echo "pwd = $(pwd)"
 
-# # run rust benchmarks
-# export API_COMMAND='./rust-api/target/release/rust-api'
-# export TEST_NAME=rust-api
-# ./run-api-benchmark.sh
+# run rust benchmarks
+export API_COMMAND='./rust-api/target/release/rust-api'
+export TEST_NAME=rust-api
+./run-api-benchmark.sh
 
 # build kotlin
 cd kotlin-api
 echo "before kotlin-api gradle build"
 ./gradlew clean build
+echo "killall java"
 killall java
 cd -
 echo "finished gradle build"
