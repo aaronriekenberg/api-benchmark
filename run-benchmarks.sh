9a242214ed6187bfe2054913968e5ffc167f60cb
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-21 04:08:24 -0600

    Run rustup update.

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -7,20 +7,22 @@ echo "begin run-benchmarks.sh"
 export OUTPUT_FILE=results/latest.md
 echo "OUTPUT_FILE=$OUTPUT_FILE"
 
-# build go
-cd go-api
-echo "before go build"
-go build
-echo "after go build"
-cd -
-echo "pwd = $(pwd)"
+# # build go
+# cd go-api
+# echo "before go build"
+# go build
+# echo "after go build"
+# cd -
+# echo "pwd = $(pwd)"
 
-# run go benchmarks
-export API_COMMAND='./go-api/go-api'
-export TEST_NAME=go-api
-./run-api-benchmark.sh
+# # run go benchmarks
+# export API_COMMAND='./go-api/go-api'
+# export TEST_NAME=go-api
+# ./run-api-benchmark.sh
 
 # build rust
+echo "rustup update"
+rustup update
 cd rust-api
 echo "before cargo build"
 cargo build --release
