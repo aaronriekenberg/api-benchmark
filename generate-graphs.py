f35a5098e27f595e1aec3bc3599a975a5439c2a9
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-12-30 06:02:48 -0600

    Rearrange generate_latest_md.

--- generate-graphs.py
+++ generate-graphs.py
@@ -229,11 +229,12 @@ def generate_latest_md(results_dir='results'):
 ### API Memory Usage
 ![API Memory MB](memory.png)
 
+### P99 Response Time
+![P99 Response Time](p99.png)
+
 ### API Threads
 ![API Threads](threads.png)
 
-### P99 Response Time
-![P99 Response Time](p99.png)
 
 ---
 *Graphs generated from benchmark results. See raw.md for detailed data.*
